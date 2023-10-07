9dc68106b02c0d45ce072c49ab6cf8bb926754e6
DawidCisinski <145262263+DawidCisinski@users.noreply.github.com>
2023-10-07 19:46:23 +0200

    Update dcs_plugin_operacji_listy.py

--- dcs_plugin_operacji_listy.py
+++ dcs_plugin_operacji_listy.py
@@ -1,5 +1,6 @@
 import os
-
+import random
+#import string
 
 def str_na_lista(str):
     if "." in str:
@@ -160,4 +161,44 @@ def zamien_na_liscie(nazwa, nazwa2, lista):
     lista[nazwa] = lista[nazwa2]
     lista[nazwa2] = tmp
     return lista
+#Dawid Csisiński 
+
+def szyfruj_tablice(tablica, klucz_szyfrowania):
+    kod_szyfrowania = ""
+    while klucz_szyfrowania > 0:
+        kod_szyfrowania += chr(klucz_szyfrowania % 256)
+        klucz_szyfrowania //= 256
+    kod_szyfrowania = kod_szyfrowania.zfill(16)
+
+    zaszyfrowana_tablica = []
+    for element in tablica:
+        zaszyfrowany_element = ""
+        for i in range(len(element)):
+            zaszyfrowany_znak = chr(ord(element[i]) ^ ord(kod_szyfrowania[i % 16]))
+            zaszyfrowany_element += zaszyfrowany_znak
+        zaszyfrowana_tablica.append(zaszyfrowany_element)
+
+    return zaszyfrowana_tablica
+
+
+def odszyfruj_tablice(tablica, klucz_szyfrowania):
+    kod_szyfrowania = ""
+    while klucz_szyfrowania > 0:
+        kod_szyfrowania += chr(klucz_szyfrowania % 256)
+        klucz_szyfrowania //= 256
+    kod_szyfrowania = kod_szyfrowania.zfill(16)
+
+    odszyfrowana_tablica = []
+    for element in tablica:
+        odszyfrowany_element = ""
+        for i in range(len(element)):
+            odszyfrowany_znak = chr(ord(element[i]) ^ ord(kod_szyfrowania[i % 16]))
+            odszyfrowany_element += odszyfrowany_znak
+        odszyfrowana_tablica.append(odszyfrowany_element)
+
+    return odszyfrowana_tablica
+
+def losowy_klucz():
+    return random.randint(0, 2 ** 128 - 1)
+
 #Dawid Csisiński 
