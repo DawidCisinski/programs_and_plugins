603345a618ea3465d82db81b33349ca46044ac5f
DawidCisinski <145262263+DawidCisinski@users.noreply.github.com>
2023-10-27 20:47:20 +0200

    Create dcscmd.py

---
+++ dcscmd.py
@@ -0,0 +1,45 @@
+def dcs_cmd(grogcom_info = "info_komend"):
+  komendy = {
+    "exit": "Wyjście z konsoli.",
+    "help": "Podpowiada komendy.",
+    "send": "Wymuszenie wysłania danych do programu.",
+    "progcom": "[help] / pozwala na komunikacje z programem matką.",
+  }
+
+  print("MicroDCS(CMD) [v.1.0]", end="\n\n")
+
+  while True:
+    wybor = input(">>>").lower().split(" ")
+
+
+    if wybor[0] == "send":
+      lista = ["send"]
+      if len(wybor) > 1:
+        if wybor[1] == "str":
+          return lista.append(wybor[2])
+        elif wybor[1] == "int":
+          return lista.append(int(wybor[2]))
+        elif wybor[1] == "float":
+          return lista.append(float(wybor[2].replace(",", ".")))
+        elif wybor[1] == "list" or wybor[1] == "bool":
+          return lista.append(eval(wybor[2]))
+
+    elif wybor[0] == "exit":
+      return None
+
+    elif wybor[0] == "progcom":
+      if len(wybor) > 1:
+        lista = []
+        if wybor[1] == "help":
+          my_string = grogcom_info.replace("<BREAK!>", "\n")
+          print(my_string)
+        else:
+          lista.append(wybor[1])
+          lista.append(wybor[2])
+          return lista
+
+    elif wybor[0] == "help":
+      print("\nkomendy:")
+      for k, v in komendy.items():
+        print(k + (12 - len(k)) * " " + v)
+      print()
